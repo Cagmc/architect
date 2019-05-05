99a8b8028d022caefff817ecf61d80ab075bdb37
Rózsa István <istvan.rozsa@outlook.com>
2019-05-05 16:06:07 +0200

    dev(Migration): add migration with aggregates

---
+++ Architect.Database/Migrations/20190505140016_AddAggregates.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Architect.Database.Migrations
+{
+    public partial class AddAggregates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AddressAggregates",
+                schema: "arc",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AggregateRootId = table.Column<int>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Zip = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddressAggregates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NameAggregates",
+                schema: "arc",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AggregateRootId = table.Column<int>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ShortName = table.Column<string>(nullable: true),
+                    LongName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NameAggregates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AddressAggregates",
+                schema: "arc");
+
+            migrationBuilder.DropTable(
+                name: "NameAggregates",
+                schema: "arc");
+        }
+    }
+}
